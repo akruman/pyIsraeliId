1a8a5f7946d203f6edf9776b69c0eee30b7ec9de
akruman <akruman@gmail.com>
2015-06-15 22:14:44 +0300

    + added all 3 algs in same file, did some timings

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # pyIsraeliId
+
+some timings:
+
+pyIsraeliId\pyIsraeliId>python checker.py
+{'ak': 0.07709081547384347, 'iluha': 0.12229156848473238, 'proind': 0.17674758141010782}
+set([('iluha', 12), ('proind', 12), ('ak', 12)])
+
+pyIsraeliId\pyIsraeliId>python checker.py
+{'ak': 0.7787547266315592, 'iluha': 1.1956536799943205, 'proind': 1.8220165022627102}
+set([('iluha', 98), ('ak', 98), ('proind', 98)])
+
+pyIsraeliId\pyIsraeliId>python checker.py
+{'ak': 0.7756871737544401, 'iluha': 1.144236567214604, 'proind': 1.786396452877376}
+set([('ak', 87), ('proind', 86), ('iluha', 86)])
--- pyIsraeliId/checker.py
+++ pyIsraeliId/checker.py
@@ -3,8 +3,15 @@ __alias__ = 'eliuha'
 
 # Shalom everybody !
 
+import re, itertools, operator
+# algorithm from : http://halemo.net/info/idcard/
+def validate_israeli_id_number_ak(iID):	
+    iID = "{:0>9d}".format(iID)
+    num_12_arr = [1, 2, 1, 2, 1, 2, 1, 2, 1]
+    digits = [int(d) for d in str(iID)]
+    return not sum((x%10+x/10 for x in itertools.imap(operator.mul, digits, num_12_arr))) % 10
 
-def validate_israeli_id_number(iID):
+def validate_israeli_id_number_iluha(iID):
     iID = str(iID)
     if len(iID) == 8:
         iID = '0' + str(iID)
@@ -26,3 +33,30 @@ def validate_israeli_id_number(iID):
         return False
 
     return True
+
+def validate_israeli_id_number_proind(iID):
+    iID = str(iID)
+    if len(iID) == 8:
+        iID = '0' + str(iID)
+    if len(iID) != 9:
+        return False
+
+    #multiply every odd index by 2
+    digits = [int(x)*(i%2+1) for (i,x) in enumerate(iID)]
+    #sum the  digits of the each item
+    digits = [sum(map(int,str(dig))) for dig in digits]
+    #sum all the digits in the list
+    sum_of_digits = sum(digits)
+    return not (sum_of_digits % 10)
+
+import random
+import timeit	
+if __name__ == "__main__":
+    ids = [random.randint(0,999999999) for r in xrange(1000)]
+    import __builtin__
+    __builtin__.__dict__.update(locals())
+    print {'ak':timeit.timeit('[validate_israeli_id_number_ak(id) for id in ids]',number=100),
+     'proind':timeit.timeit('[validate_israeli_id_number_proind(id) for id in ids]',number=100),
+     'iluha':timeit.timeit('[validate_israeli_id_number_iluha(id) for id in ids]',number=100)}
+    validators = {'iluha':validate_israeli_id_number_iluha, 'proind':validate_israeli_id_number_proind, 'ak':validate_israeli_id_number_ak }
+    print {(k,sum([validators[k](id) for id in ids])) for k in validators}
